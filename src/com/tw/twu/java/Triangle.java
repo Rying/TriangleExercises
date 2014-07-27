82cf0cd719cb056553eb3abaf6bb8a02c97ae2f8
yren <renying0718@gmail.com>
2014-07-27 11:02:06 +0800

    Add function--draw a right triangle

--- src/com/tw/twu/java/Triangle.java
+++ src/com/tw/twu/java/Triangle.java
@@ -8,6 +8,7 @@ public class Triangle {
     public void HorizontalLine(int number) {
         while (number-- > 0)
             System.out.print("*");
+        System.out.println();
     }
 
     public void VerticalLine(int number) {
@@ -15,8 +16,22 @@ public class Triangle {
             System.out.println("*");
     }
 
+    public void RightTriangle(int number) {
+        int index = 0;
+        while (index++ < number)
+            HorizontalLine(index);
+        System.out.println();
+    }
+
     public static void main(String[] args) {
         Triangle triangle = new Triangle();
+        System.out.println("Easiest exercise ever");
+        triangle.Easiest();
+        System.out.println("Draw a horizontal line");
+        triangle.HorizontalLine(3);
+        System.out.println("Draw a vertical line");
         triangle.VerticalLine(8);
+        System.out.println("Draw a right triangle");
+        triangle.RightTriangle(3);
     }
 }
